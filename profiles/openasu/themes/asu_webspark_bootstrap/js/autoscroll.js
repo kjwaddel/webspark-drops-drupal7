457281501a36e7f02fab17b7322b5741cca0a8a3
Ariel Gold <agold2@asu.edu>
2014-02-25 10:46:42 -0700

    Change transition time from .5seconds to 0seconds

--- profiles/openasu/themes/asu_webspark_bootstrap/js/autoscroll.js
+++ profiles/openasu/themes/asu_webspark_bootstrap/js/autoscroll.js
@@ -11,7 +11,7 @@
               $('html, body').animate({
                 scrollTop: $("#asu_mobile_hdr").offset().top
               }, 1000);
-            }, 500);
+            }, 0);
             $this.data('clickNum', ++clickNum);
           }
         });
